1772df63860b6d2a4afb03a1051d4115999bb360
uberj <uberj@onid.orst.edu>
2013-08-28 12:56:34 -0700

    renamed some form fields and buttons

--- core/range/forms.py
+++ core/range/forms.py
@@ -13,6 +13,8 @@ class RangeForm(forms.ModelForm):
 
     def __init__(self, *args, **kwargs):
         super(RangeForm, self).__init__(*args, **kwargs)
+        self.fields['start_str'].label = "Start Address"
+        self.fields['end_str'].label = "End Address"
         #self.fields['dhcpd_raw_include'].label = "DHCP Config Extras"
         #self.fields['dhcpd_raw_include'].widget.attrs.update(
         #    {'cols': '80', 'style': 'display: none;width: 680px'})
